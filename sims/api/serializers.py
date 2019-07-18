95a624642994899e91dd273c1335b061c820a22f
adam <amschaal@gmail.com>
2019-07-18 16:28:28 -0700

    Make it possible to deserialize full objects but serialize using just primary key

--- sims/api/serializers.py
+++ sims/api/serializers.py
@@ -1,6 +1,34 @@
 from rest_framework import serializers
 from sims.models import Project, Machine, Run, Sample, Adapter, Library, Pool, RunPool
 
+#Allows Creation/Updating of related model fields with OBJECT instead of just id
+#usage field_name = ModelRelatedField(model=Sample,serializer=SampleSerializer)
+class ModelRelatedField(serializers.RelatedField):
+    model = None
+    pk = 'id'
+    serializer = None
+    def __init__(self, **kwargs):
+        self.model = kwargs.pop('model', self.model)
+        self.pk = kwargs.pop('pk', self.pk)
+        self.serializer = kwargs.pop('serializer', self.serializer)
+        assert self.model is not None, (
+            'Must set model for ModelRelatedField'
+        )
+        assert self.serializer is not None, (
+            'Must set serializer for ModelRelatedField'
+        )
+        self.queryset = kwargs.pop('queryset', self.model.objects.all())
+        super(ModelRelatedField, self).__init__(**kwargs)
+    def to_internal_value(self, data):
+        if isinstance(data, int) or isinstance(data, str):
+            kwargs = {self.pk:data}
+            return self.model.objects.get(**kwargs)
+        if data.get(self.pk,None):
+            return self.model.objects.get(id=data['id'])
+        return None
+    def to_representation(self, value):
+        return self.serializer(value).data
+
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
