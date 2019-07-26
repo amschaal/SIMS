ed1c54c39ccd0bd908e1d3615a86d28a394d990c
adam <amschaal@gmail.com>
2019-07-26 16:38:21 -0700

    fix pagination

--- sims/settings.py
+++ sims/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend',
                                 'rest_framework.filters.OrderingFilter',
                                 'rest_framework.filters.SearchFilter'], #['rest_framework_filters.backends.RestFrameworkFilterBackend']
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'sims.api.pagination.StandardPagePagination',
     'PAGE_SIZE': 10
 }
 
