5968f319dadaf1832d9305c510a97641e49f7fd6
adam <amschaal@gmail.com>
2019-07-31 14:09:46 -0700

    Working on better dialogs for forms

---
+++ spa/src/components/dialogs/BaseFormDialog.vue
@@ -0,0 +1,42 @@
+<template>
+  <BaseDialog ref="dialog" :title="title">
+    <template v-slot:content>
+      {{formComponent}}Foo!
+      <component :is="formComponent" ref="form">
+        <template v-slot:buttons></template>
+      </component>
+    </template>
+    <template v-slot:buttons>
+      <q-btn flat label="Submit" color="primary" @click="submit"/>
+      <q-btn flat label="Cancel" color="primary" v-close-popup />
+    </template>
+  </BaseDialog>
+</template>
+<script>
+import BaseDialog from './BaseDialog.vue'
+// import Vue from 'vue'
+// import _ from 'lodash'
+export default {
+  props: ['formComponent', 'title'],
+  data () {
+    return {
+      errors: {},
+      data: this.model
+    }
+  },
+  methods: {
+    open () {
+      this.$refs.dialog.open()
+    },
+    submit () {
+      this.$refs.form.submit()
+    }
+  },
+  // mounted: function () {
+  //
+  // },
+  components: {
+    BaseDialog
+  }
+}
+</script>
