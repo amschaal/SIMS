9ed5b17f80dbb88c1b4920003ed3c6b1b66867a9
adam <amschaal@gmail.com>
2019-08-06 16:17:45 -0700

    project formatting

--- spa/src/components/details/Project.vue
+++ spa/src/components/details/Project.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-md-4 col-sm-6"><b>Project ID: </b>{{project.id}}</div>
-      <div class="col"><b>Submission ID: </b><a :href="project.submission_id">{{project.submission_id}}</a></div>
+      <div class="col-md-4 col-sm-12"><b>Project ID: </b>{{project.id}}</div>
+      <div class="col-md-4 col-sm-12"><b>Submission ID: </b><a :href="project.submission_id">{{project.submission_id}}</a></div>
+      <div class="col-md-4 col-sm-12"><b>Type: </b>{{project.type.name}}</div>
     </div>
     <div class="row">
-      <div class="col-md-4 col-sm-6"><b>PI: </b>{{project.pi_first_name}} {{project.pi_last_name}} ({{project.pi_email}})</div>
-      <div class="col"><b>Submitter: </b>{{project.first_name}} {{project.last_name}} ({{project.email}})</div>
+      <div class="col-md-4 col-sm-12"><b>PI: </b>{{project.pi_first_name}} {{project.pi_last_name}} ({{project.pi_email}})</div>
+      <div class="col-md-4 col-sm-12"><b>Submitter: </b>{{project.first_name}} {{project.last_name}} ({{project.email}})</div>
+      <div class="col-md-4 col-sm-12"><b>Institute: </b>{{project.institute}})</div>
+      <div class="col-12"><b># Samples: </b>{{project.sample_data.length}}</div>
+      <div class="col-12"><b>Comments: </b>{{project.comments}})</div>
     </div>
   </div>
 </template>
