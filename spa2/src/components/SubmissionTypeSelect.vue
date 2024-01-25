548b4357a498afabaf7f5ff9c693ff1895e9046a
Adam Schaal <amschaal@gmail.com>
2024-01-25 10:00:41 -0800

    Vue component for selecting submission type #8

---
+++ spa2/src/components/SubmissionTypeSelect.vue
@@ -0,0 +1,32 @@
+<template>
+    <q-select outlined v-model="model" :options="options" label="Type" ref="select"
+        option-value="id"
+        option-label="name"
+        @update:modelValue="val => selected(val)"
+    />
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  data () {
+    return {
+      model: this.modelValue,
+      options: []
+    }
+  },
+  methods: {
+    selected (val) {
+      this.$emit('update:modelValue', val)
+    }
+  },
+  mounted: function () {
+    this.$api
+      .get('/api/submission_types/get_submission_types/')
+      .then(response => {
+        this.options = response.data
+      })
+  }
+}
+</script>
