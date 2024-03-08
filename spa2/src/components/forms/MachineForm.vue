ce435d66eaf6192a3a79287fcab309a386a106ff
Adam Schaal <amschaal@gmail.com>
2024-03-08 13:47:14 -0800

    switch machineform to JSONTypeForm

--- spa2/src/components/forms/MachineForm.vue
+++ spa2/src/components/forms/MachineForm.vue
@@ -1,49 +1,46 @@
 <template>
-  <JSONForm
-    ref="form"
-    api-method="post"
-    api-url="/api/machines/"
-    :on-success="onSuccess"
-    :on-error="onError"
-    :hide-buttons="hideButtons"
-    :model="model"
+  <JSONTypeForm
+    v-model="model"
+    :errors="errors"
   >
-    <template v-slot:content="{ model, _errors, has_error }">
-      Errors: {{ _errors }}
+    <template v-slot:content="{ model, errors, has_error }">
+      Model: {{ model }}
+      Errors: {{ errors }}
+
       <!-- <q-input outlined v-model="model.type" label="Type"
         :error-message="errors.type"
         :error="has_error.type"
         /> -->
       <q-input outlined v-model="model.id" label="ID"
-        :error-message="_errors.id"
+        :error-message="errors.id"
         :error="has_error.id"
         />
       <q-input outlined v-model="model.name" label="Name"
-        :error-message="_errors.name"
+        :error-message="errors.name"
         :error="has_error.name"
         />
       <q-input outlined v-model="model.description" label="Description" />
       <q-input outlined v-model="model.num_lanes" type="number" label="Number of Cells/Lanes"
-        :error-message="_errors.num_lanes"
+        :error-message="errors.num_lanes"
         :error="has_error.num_lanes"
       />
       {{ model.data }}
     </template>
-  </JSONForm>
+  </JSONTypeForm>
 </template>
 <script>
-import JSONForm from './JSONForm.vue'
+import JSONTypeForm from './JSONTypeForm.vue'
 export default {
-  props: ['onSuccess', 'onError', 'hideButtons'],
+  props: ['errors', 'modelValue'],
   data () {
     return {
-      model: { data: { foo: 'baz' }, type: 'machine_illumina_hiseq' }
+      model: this.modelValue
     }
   },
   methods: {
   },
   components: {
-    JSONForm
+    JSONTypeForm
   }
 }
 </script>
--- spa2/src/pages/MachinesPage.vue
+++ spa2/src/pages/MachinesPage.vue
@@ -1,7 +1,12 @@
 <template>
   <q-page class="q-pa-sm q-gutter-sm">
     <h4 class="text-center">Machines</h4>
-    <BaseFormDialog ref="machine_form" :form-component="MachineForm" title="Add Machine" :on-success="machineAdded" :on-error="machineError"/>
+    <!-- <BaseFormDialog ref="machine_form" :form-component="MachineForm" title="Add Machine" :on-success="machineAdded" :on-error="machineError" :model="{}"/> -->
+    <FormDialog ref="form_dialog" title="Create Machine" :on-success="machineAdded" :on-error="machineError" api-method="POST" api-url="/api/machines/">
+      <template #form="props">
+        <MachineForm v-model="props.data" :errors="props.errors"/>
+      </template>
+    </FormDialog>
     <q-btn label="Add Machine" color="primary" @click="addMachine"/>
     <!-- <RunForm/> -->
     <MachinesTable/>
@@ -14,22 +19,20 @@
 <script>
 import MachinesTable from '../components/tables/MachinesTable.vue'
 import MachineForm from '../components/forms/MachineForm.vue'
-import BaseFormDialog from '../components/dialogs/BaseFormDialog.vue'
+import FormDialog from '../components/dialogs/FormDialog.vue'
 export default {
   data () {
     return {
-      MachineForm
     }
   },
   methods: {
     addMachine () {
-      this.$refs.machine_form.open()
+      this.$refs.form_dialog.open()
     },
     machineAdded (request) {
       console.log('machine!', request.data)
       this.$q.notify('Machine added.')
       this.$router.push({ name: 'machine', params: { id: request.data.id } })
-      this.$refs.machine_form.$refs.dialog.close()
     },
     machineError (error) {
       if (error) {
@@ -39,8 +42,8 @@ export default {
   },
   components: {
     MachinesTable,
-    // RunForm,
-    BaseFormDialog
+    MachineForm,
+    FormDialog
   }
 }
 </script>
