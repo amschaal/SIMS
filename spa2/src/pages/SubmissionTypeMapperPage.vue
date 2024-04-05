2f612824ef3723cc51c9ba0adcd6e71ac18a65d0
Adam Schaal <amschaal@gmail.com>
2024-04-05 16:09:18 -0700

    work on mapper #19

--- spa2/src/pages/SubmissionTypeMapperPage.vue
+++ spa2/src/pages/SubmissionTypeMapperPage.vue
@@ -1,6 +1,7 @@
 <template>
   <q-page class="q-pa-sm q-gutter-sm" v-if="mapper">
     <h6 class="text-center"><router-link :to="{ name: 'submission_type',  params: { id: mapper.submission_type.id } }">{{ mapper.submission_type.name }}</router-link> / Mappers / {{mapper.name}}</h6>
+    <DeleteButton :url="`/api/submission_type_mappers/${id}/`"/>
     <q-tabs
         v-model="tab"
       >
@@ -12,7 +13,8 @@
             From: {{ mapper.submission_type.name }}
             To: {{mapper.model_type.name}}
     <!-- Variables: {{  schema_to_variables(submission_type.submission_schema) }} -->
-    <DataMapper v-model="mapper.mapping" :submission_type="mapper.submission_type" :type="mapper.model_type"/>
+            <DataMapper v-model="mapper.mapping" :submission_type="mapper.submission_type" :type="mapper.model_type"/>
+            <q-btn label="Update Mapping" @click="updateMapping"/>
             <!-- {{mapper}} -->
             {{ mapper.mapping }}
           </q-tab-panel>
@@ -27,6 +29,7 @@
 
 <script>
 import DataMapper from 'src/components/DataMapper.vue'
+import DeleteButton from 'src/components/DeleteButton.vue'
 export default {
   name: 'SubmissionTypeMapperPage',
   props: ['id'],
@@ -44,8 +47,20 @@ export default {
         self.mapper = response.data
       })
   },
+  methods: {
+    updateMapping () {
+      this.$api
+        .put(`/api/submission_type_mappers/${this.id}/`, this.mapper)
+        .then(response => {
+          this.$q.notify({ message: 'Mapper updated.', type: 'positive' })
+          // this.options = response.data.results
+          // this.submission_type.mapping = this.mapping
+        })
+    }
+  },
   components: {
-    DataMapper
+    DataMapper,
+    DeleteButton
   }
 }
 </script>
