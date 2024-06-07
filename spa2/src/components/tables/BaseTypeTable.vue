38ca7351f2e819a4d2a1065d38f9427eba3e1dbd
Adam Schaal <amschaal@gmail.com>
2024-06-07 16:03:06 -0700

    work on adding custom fields to search table #29

--- spa2/src/components/tables/BaseTypeTable.vue
+++ spa2/src/components/tables/BaseTypeTable.vue
@@ -7,11 +7,16 @@
     :filters="filters"
     ref="table"
   >
+    <template v-slot:top-left>
+      <TypeSelect :dense="true" :error_messages="{}" :has_error="false" v-model="type" :emit_object="true"  :model-filter="'sample'"/>
+      {{ type_columns }}
+      <!-- <TypeSelect :dense="true" :modelValue="mapping[variable].model_type" @update:model-value="val => selectedModelType(val, variable)" :emit_object="true" v-if="mapping[variable] && mapping[variable].mapping_type === 'model'" :model-filter="mapping[variable].model"/> -->
+    </template>
     <template v-slot:body="{ props }">
       <q-tr :props="props">
         <slot name="columns" v-bind="{ props }"/>
         <q-td key="type" :props="props"><Property :value="props.row.type" label="name"/></q-td>
-        <q-td :key="col.id" v-for="col in type_columns" :label="col.label">{{ props.row.data[col.name] }}</q-td>
+        <q-td :key="col.name" v-for="col in type_columns" :label="col.label">{{ props.row.data[col.name] }}</q-td>
       </q-tr>
     </template>
   </BaseTable>
@@ -21,18 +26,25 @@
 
 import Property from '../details/Property.vue'
 import BaseTable from './BaseTable.vue'
+import TypeSelect from 'src/components/TypeSelect.vue'
 
 export default {
   name: 'SamplesTable',
-  props: ['filters', 'options', 'columns', 'visibleColumns', 'apiUrl', 'type'],
+  props: ['filters', 'options', 'columns', 'visibleColumns', 'apiUrl'],
   data () {
     return {
-      combined_options: this.options ? this.options : {}
+      combined_options: this.options ? this.options : {},
+      type: null
     }
   },
   computed: {
     type_columns () {
-      return [{ name: 'sample_name', label: 'Sample Name', field: 'data.sample_name', sortable: true }]
+      if (!this.type || !this.type.schema || !this.type.schema.order) {
+        return []
+      } else {
+        return this.type.schema.order.map(v => ({ name: v, label: v, field: `data.${v}`, sortable: false })) // this.type.schema.properties[v].title
+      }
+      // return [{ name: 'sample_name', label: 'Sample Name', field: 'data.sample_name', sortable: true }]
     },
     combined_columns () {
       return this.columns.concat(this.type_columns)
@@ -40,7 +52,8 @@ export default {
   },
   components: {
     BaseTable,
-    Property
+    Property,
+    TypeSelect
   }
 }
 
